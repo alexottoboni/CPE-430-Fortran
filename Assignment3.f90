5175a447becc58dc2ce13ae67777d8af46a90ea5
Alex Ottoboni <alexottoboni@gmail.com>
2016-05-24 12:21:48 -0700

    Started Assignment 3

---
+++ Assignment3.f90
@@ -0,0 +1,12 @@
+program Assignment3
+TYPE :: Binop
+   character(1) :: Op
+   integer :: Left
+   integer :: Right
+END TYPE Binop
+
+TYPE :: IdC
+   character(20) :: id
+END TYPE
+
+end program Assignment3
